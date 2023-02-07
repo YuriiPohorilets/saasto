09fce71bd39e0cff9b0e2e959736f58a21cc204e
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2023-02-07 22:44:49 +0200

    added global styled, mixins, variables

---
+++ src/js/menu.js
@@ -0,0 +1,13 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-btn]');
+
+  const toggle = () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    // document.body.classList.toggle('menu-is-open');
+  };
+
+  menuBtnRef.addEventListener('click', toggle);
+})();
