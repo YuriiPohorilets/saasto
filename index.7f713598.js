b881ebbf2ea7449ba3e92e8e2ff445bb943d4b85
YuriiPohorilets <102477210+YuriiPohorilets@users.noreply.github.com>
2023-02-19 13:03:59 +0000

    Deploying to gh-pages from @ YuriiPohorilets/saasto@2b37dffbd062f3579629bac15afbc0cca3419262 🚀

--- index.7f713598.js
+++
@@ -1,2 +0,0 @@
-!function(){var e;(e=document.querySelector("[data-menu-btn]")).addEventListener("click",(function(){var t="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!t)}))}();
-//# sourceMappingURL=index.7f713598.js.map
